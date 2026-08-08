8edd826b15b928859bca523e732a23cd76b796ae
kachgundevaibhav7561-design <kachgundevaibhav7561@gmail.com>
2026-08-08 12:34:28 +0530

    changes

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ window.addEventListener("scroll", function(){
 
     let navbar = document.querySelector(".navbar");
 
-    if(window.scrollY > 50){
+    if(window.scrollY > 100){
 
         navbar.classList.add("scrolled");
 
